52742285d1675f6b24e96d85829693976bea4fb4
RichardJanebo <richardjanebo@gmail.com>
2024-04-09 08:05:10 -0300

    memory game part III

---
+++ memoryGame/js/loginMemorygame.js
@@ -0,0 +1,24 @@
+const nameLogin = document.querySelector("#tex_input")
+const btnLogin = document.querySelector("#btn_login")
+const form = document.querySelector("#login_form")
+
+const validateInput = ({target})=>{
+    if (target.value.length > 3){
+        btnLogin.removeAttribute("disabled")
+    }else{
+        btnLogin.setAttribute("disabled","disabled")
+    }
+}
+
+
+const handleSubmit = (evt)=>{
+   evt.preventDefault()
+   localStorage.setItem("player",nameLogin.value)
+   window.location = "pages/memoryG.html"
+
+}
+
+
+nameLogin.addEventListener("input",validateInput)
+
+form.addEventListener("submit", handleSubmit)
