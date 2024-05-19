c57e2351a8154c1c06e33cf2c017cbb9614ff41d
RichardJanebo <richardjanebo@gmail.com>
2024-05-19 12:14:18 -0300

    Rick and morty II

--- rickandmortyApi/pages/searchRickAndeMorty.js
+++ rickandmortyApi/pages/searchRickAndeMorty.js
@@ -1,20 +1,85 @@
 const search = document.querySelector("#search")
 const input_search = document.querySelector("#input_search")
-const img =  document.querySelector("#img")
+const img = document.querySelector("#img")
+const stage = document.querySelector("#stage")
 
-search.addEventListener("click",()=>{
-    console.log(Number(input_search.value))
-    fetchApi(Number(input_search.value))
+search.addEventListener("click", () => {
+    fetchApi(input_search.value)
 })
 
+const selectOpition = ()=>{
+    let indice = 1
+    const endpoint = fetch(`https://rickandmortyapi.com/api/character/`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((response) => {
+            while(indice < response.info.count){
+                const option = document.createElement("option")
+                option.setAttribute("value",indice)
+                fetch(`https://rickandmortyapi.com/api/character/${indice}`)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((res)=>{
+                    option.innerHTML=String(res.name)
+                    input_search.appendChild(option)
+                })
+                
 
-const fetchApi =(p)=>{
-    fetch(`https://rickandmortyapi.com/api/character/${p}`)
-    .then((response)=>{
-        return response.json()
-    })
-    .then((response)=>{
-        console.log(response)
-        img.setAttribute("src",response.image)
-    })
+                indice++
+            }
+            
+        })
 }
+
+
+
+const createData = (p) => {
+    stage.innerHTML = ""
+    let data = {
+        name: `Nome: ${p.name}</br>`,
+        episodes: `Episódios: ${p.episode.length}</br>`,
+        gender: `Genero: ${p.gender != "Male" ? "Feminino" : "Masculino"}</br>`,
+        species: `Especie: ${p.species}</br>`,
+    }
+    const div = document.createElement("div")
+    const img = document.createElement("img")
+    img.setAttribute("src", p.image)
+
+    div.innerHTML = data.name
+    div.innerHTML += data.gender
+    div.innerHTML += data.species
+    div.innerHTML += data.episodes
+
+    stage.appendChild(img)
+    stage.appendChild(div)
+
+
+}
+
+
+
+
+const fetchApi = (p) => {
+    if(p == 0 ){
+        alert("Escolha pelo menos um personagem")
+    }
+    fetch(`https://rickandmortyapi.com/api/character/${p}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((response) => {
+            console.log(response)
+            createData(response)
+
+
+        })
+
+}
+
+
+window.addEventListener("load",()=>{
+    selectOpition()
+    fetchApi(1)
+})
