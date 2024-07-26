5af3b7e5757c87d4380710eca80cbd7a5a565498
RichardJanebo <richardjanebo@gmail.com>
2024-07-26 08:01:33 -0300

    Spring com H2

---
+++ webApijava/userdept/src/main/java/com/webapp/userdept/entitities/Department.java
@@ -0,0 +1,37 @@
+package com.webapp.userdept.entitities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+    public Department(){}
+
+
+    public String getName(){
+        return name;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setName(String name){
+        this.name =  name;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+}
